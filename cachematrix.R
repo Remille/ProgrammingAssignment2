695e063325b46a773f34e208891ac2ab712394cf
remille <sophia.munshi@yahoo.com>
2016-02-14 09:32:39 -0500

    added comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,50 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+#helper functions for setting and getting the solution
+#of a matrix
 makeCacheMatrix <- function(x = matrix()) {
-
+  soln <- NULL
+  set <- function(y) {
+    x <<- y
+    soln <<- NULL
+  }
+  
+  get <- function() {
+    x
+  }
+  
+  setsoln <- function(newSoln) {
+    soln <<- newSoln
+  }
+  
+  getsoln <- function() {
+    soln
+  }
+  
+  out <- c(set = set, 
+           get = get, 
+           setsoln = setsoln,
+           getsoln = getsoln)
+  matrix(out)
 }
 
+#if the solution of a matrix is not set,
+#calulate the matrix solution and set it
+#if it is saved, access the saved solution
 
-## Write a short comment describing this function
-
+#cacheSolve takes as its argument x, the helper function
+#the helper function takes the matrix as its argument
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  #x refers to the matrix returned by the helper function
+  #it is a matrix containg function calls
+  m <- x[[4]]() #calls getsoln()
+  if(!is.null(m))  {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x[[2]]() #calls get()
+  #if no matrix is specified to the helper function, 
+  #the function defaults to an empty matrix
+  m <- solve(data)
+  x[[3]](m) #calls setsoln()
+  #returns matrix solution
+  m
 }
